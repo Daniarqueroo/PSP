8f0ce572caac068e4f62a14597c6fe06cd408b80
Daniarqueroo <daniarquero@gmail.com>
2022-10-13 09:23:20 +0200

    update

--- P3/src/Main.java
+++ P3/src/Main.java
@@ -1,8 +1,11 @@
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ProcessBuilder pb = new ProcessBuilder(args);
+        ProcessBuilder pb = new ProcessBuilder("cmd", "/C",
+                "\"C:\\Program Files\\JetBrains\\IntelliJ IDEA Community Edition 2022.2.1\\jbr\\bin\\java.exe\" P2 -cp " +
+                        "'C:\\Users\\Usuario DAM2\\Documents\\GitHub\\PSP\\P3\\src'");
         pb.start();
     }
 }
